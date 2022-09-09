f36fea19d6b7d5bccefd2617b6ba0d0e731f3f97
Yipai Du <yipai.du@outlook.com>
2022-09-09 16:18:23 -0400

    add params for digit create dataset, update training

--- pytact/models/pixel2grad.py
+++ pytact/models/pixel2grad.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
-import torch.nn.functional as F_
 from pytact.types import ModelType
+
 
 class Pixel2GradModel(nn.Module):
     """
@@ -8,25 +8,25 @@ class Pixel2GradModel(nn.Module):
 
     Architecture: 5 (R, G, B, x, y) -> 64 -> 64 -> 64 -> 2 (gx, gy)
     """
-    
+
     model_type = ModelType.Pixel2Grad
     dropout_p = 0.05
 
-    def __init__(self):
+    def __init__(self, hidden_size=64, dropout_p=0.05, activation=nn.ReLU(True)):
         super().__init__()
+        self.activation = activation
 
-        self.fc1 = nn.Linear(5, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 2)
-        self.drop = nn.Dropout(p=self.dropout_p)
+        self.fc1 = nn.Linear(5, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, 2)
+        self.drop = nn.Dropout(p=dropout_p)
 
     def forward(self, x):
-        x = F_.relu(self.fc1(x))
+        x = self.activation(self.fc1(x))
         x = self.drop(x)
-        x = F_.relu(self.fc2(x))
+        x = self.activation(self.fc2(x))
         x = self.drop(x)
-        x = F_.relu(self.fc3(x))
+        x = self.activation(self.fc3(x))
         x = self.drop(x)
         return self.fc4(x)
- 
